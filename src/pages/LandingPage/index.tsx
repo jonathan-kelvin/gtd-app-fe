4755b4737ce2b9f832f8db22ac46aa61f10b9f0b
jonathan-kelvin <jonathankelvinsantoso@gmail.com>
2023-05-30 22:13:41 +0800

    minor tweaks

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -7,6 +7,24 @@ const LandingPage: React.FC = () => {
       <div className="flex justify-center items-center">
         <img className="w-[48rem]" src={Image01} alt="landing page logo" />
       </div>
+      <p className="text-custom_white font-oxygen">
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum."
+      </p>
+      <p className="text-custom_white">
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum."
+      </p>
       <p className="text-custom_white font-poppins">
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,7 @@ export default {
       },
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
+        oxygen: ['Oxygen', 'sans-serif'],
       },
     },
   },
